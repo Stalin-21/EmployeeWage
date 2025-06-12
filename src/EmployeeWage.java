d64d93f8a6268de6e3e6b5d397fab321b335f9ea
stalin-21 <stalinguna21@gmail.com>
2025-06-12 13:06:08 +0530

    print statement changed

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -2,6 +2,6 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage");
 
-        System.out.println("UC1");
+        System.out.println("UC1 changed");
     }
 }
