f5a623c1764f0231a3d10f83b041c46ea5ef632e
stalin-21 <stalinguna21@gmail.com>
2025-06-12 21:43:39 +0530

    UC4 - Switch case implementation completed

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -11,20 +11,23 @@ public class EmployeeWage {
 
         int attendance = random.nextInt(3); // 0-absent // 1 - present full time // 2 - present part time
 
-        if(attendance == 0){
-            System.out.println("Employee not present");
-        }else if(attendance == 1){
-            System.out.println("Employee present");
-        }else {
-            System.out.println("Part time Employee present");
-        }
+     switch (attendance){
+         case 0:
+             System.out.println("Employee absent");
+             break;
+         case 1:
+             System.out.println("Employee present: Full time");
+             dailyWage = wagePerHour * fullDayHour;
+             break;
+         case 2:
+             System.out.println("Employee present: Part time");
+             dailyWage = wagePerHour * partTimeHour;
+             break;
 
-
-        if(attendance == 1){
-            dailyWage = fullDayHour*wagePerHour;
-        } else if (attendance == 2) {
-            dailyWage = partTimeHour*wagePerHour;
-        }
+             default:
+                 System.out.println("Invalid attendance");
+                 break;
+     }
 
         System.out.println("Daily Wage: "+dailyWage);
     }
