99aff46b505a56553cb0ac7ecaa008687e15ab8d
stalin-21 <stalinguna21@gmail.com>
2025-06-12 20:55:09 +0530

    UC3 - Part time emploee wage calculation completed

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -5,22 +5,27 @@ public class EmployeeWage {
 
         int wagePerHour = 20;
         int fullDayHour = 8;
+        int partTimeHour = 4;
         int dailyWage =0;
         Random random = new Random();
 
-        int attendance = random.nextInt(2);
+        int attendance = random.nextInt(3); // 0-absent // 1 - present full time // 2 - present part time
 
-       if(attendance == 0){
-           System.out.println("Employee not present");
-       }else{
-           System.out.println("Employee present");
-       }
+        if(attendance == 0){
+            System.out.println("Employee not present");
+        }else if(attendance == 1){
+            System.out.println("Employee present");
+        }else {
+            System.out.println("Part time Employee present");
+        }
 
 
-       if(attendance == 1){
-           dailyWage = fullDayHour*wagePerHour;
-       }
+        if(attendance == 1){
+            dailyWage = fullDayHour*wagePerHour;
+        } else if (attendance == 2) {
+            dailyWage = partTimeHour*wagePerHour;
+        }
 
-       System.out.println("Daily Wage: "+dailyWage);
+        System.out.println("Daily Wage: "+dailyWage);
     }
 }
