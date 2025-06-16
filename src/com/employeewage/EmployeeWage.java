685af8c57da016f47f3a801f17d61f54755920ef
stalin-21 <stalinguna21@gmail.com>
2025-06-16 14:39:59 +0530

    UC14: Implemented method to retrieve total wage by company name

--- src/com/employeewage/EmployeeWage.java
+++ src/com/employeewage/EmployeeWage.java
@@ -5,9 +5,10 @@ public class EmployeeWage {
         System.out.println("Welcome to Employee Wage Computation Program.");
         System.out.println("Computing wages for multiple companies:\n");
 
-        IComputeEmpWage empWageBuilder = new EmpWageBuilder();
+        EmpWageBuilder empWageBuilder = new EmpWageBuilder();
         empWageBuilder.addCompanyEmpWage("TCS", 20, 8, 4, 20, 100);
         empWageBuilder.addCompanyEmpWage("Infosys", 25, 8, 4, 22, 110);
         empWageBuilder.computeEmpWage();
+        System.out.println("Queried Wage for TCS: " + empWageBuilder.getTotalWageByCompany("TCS"));
     }
 }
